5967ddc3d23c980277561aa5186a34b26901b88a
Alex <98064532+StarMan3@users.noreply.github.com>
2024-03-05 11:21:09 -0500

    Add files

---
+++ main.js
@@ -0,0 +1,37 @@
+// main.js
+
+document.getElementById('filterForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var minYear = document.getElementById('minYear').value;
+    var maxYear = document.getElementById('maxYear').value;
+    // Get other filter values here
+    var filteredCars = usedCars.filter(function(car) {
+        return car.year >= minYear && car.year <= maxYear;
+        // Add other filter conditions here
+    });
+    displayCars(filteredCars);
+});
+
+function displayCars(cars) {
+    console.log("displayCars called");
+    var carList = document.getElementById('carList');
+    carList.innerHTML = '';
+    cars.forEach(function(car) {
+        // let carImg = car.make + car.model + ".jpg";
+        let carImg = `./assets/img/${car.make} ${car.model}.jpg`;
+        var carElement = document.createElement('div');
+        carElement.classList.add('car');
+        carElement.innerHTML = `
+            <h2>${car.make} ${car.model}</h2>
+            <img src="${carImg}" alt="${car.make} ${car.model}">
+            <p>Year: ${car.year}</p>
+            <p>Mileage: ${car.mileage}</p>
+            <p>Price: ${car.price}</p>
+            <p>Color: ${car.color}</p>
+        `;
+        carList.appendChild(carElement);
+    });
+}
+
+// Initial display of all cars
+displayCars(usedCars);
